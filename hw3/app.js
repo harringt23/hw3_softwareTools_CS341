51f3ccc2e5a3fca53e01a43016a15d50c1a0c9ad
BH <74618590+harringt23@users.noreply.github.com>
2022-02-06 23:21:04 -0800

    begin HW4

--- hw3/app.js
+++ hw3/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// added new router to respond to orders
+// var ordersRouter = require('./routes/orders');
 
 var app = express();
 
@@ -21,6 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// respond the local host address
+// app.use('/orders', ordersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
