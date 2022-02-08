b419003d7d1d182eddccf38bbd5a2e8364774238
BH <74618590+harringt23@users.noreply.github.com>
2022-02-07 22:24:36 -0800

    w

--- hw3/public/javascripts/cheesecake_functions.js
+++ hw3/public/javascripts/cheesecake_functions.js
@@ -7,7 +7,7 @@
 
 // Create a handler for when a submit event is signaled
 submitHandler = function(e) {
-	var notes = $.trim($("#notes").val());            
+	var notes = $.trim($("#notesText").val());            
 	var vegan = notes.toLowerCase().indexOf("vegan");
 	var warning = "Warning, these cheesecakes contain dairy. Do you still want to order?";
 	// Determine whether vegan was in the description
@@ -16,16 +16,16 @@ submitHandler = function(e) {
 		if(!confirm(warning)){
 				// Cancel the order if decline
 				alert("Order successfully canceled. Have a nice day!");
-				return false;
+				//return false;
 	}
 	// checking to see if notes contain the word "vegan"
 	else{
 		// Hide the forms
-		$("h3").hide();
-		$("textarea").hide();
-		$("form").hide();	       
-		$("ul").hide();
-		$("input").hide();
+		// $("h3").hide();
+		// $("textarea").hide();
+		// $("form").hide();	       
+		// $("ul").hide();
+		// $("input").hide();
 
 		// Alert the users of what they ordered
 		alert("Thank you! Your order has been placed. Here are the details:"
