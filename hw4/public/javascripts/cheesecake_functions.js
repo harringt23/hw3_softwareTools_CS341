35f22a253abd4bc0e8283df084fd68be31bde01b
BH <74618590+harringt23@users.noreply.github.com>
2022-02-08 03:28:11 -0800

    cleanup

--- hw4/public/javascripts/cheesecake_functions.js
+++ hw4/public/javascripts/cheesecake_functions.js
@@ -12,9 +12,10 @@
  *
  ***/
 
-/* handler to help with the tasks needed to use the submit event */
+/* Handler to help with the tasks needed to use the submit event */
 submitHandler = function (e) {
-    // extract
+    // extract the values of the text and "vegan" key-phrase 
+    //TODO convert back to case insensitive 
     var notesText = document.getElementById('notes').value;
     var veg = 'vegan';
 
@@ -24,15 +25,18 @@ submitHandler = function (e) {
     }
     // otherwise submit the entries and hide the elements no longer needing to be used
     else {
+        // hide unnecessary elements
         $("#table").hide();
         $("#notes").hide();
         $("#notesText").hide();
         $("#order").hide();
 
+        // determine the values of necessary items / write thank you message
         var topping = $("input[name='topping']:checked").val();
         var quantity = $('#quantity :selected').text();
-        var thankyou = "<h3>Thank you! Your order has been placed</h3>";
+        var message = "<h3>Thank you! Your order has been placed</h3>";
 
+        // write an alert for the parameters
         var toppingsAlert = "<p>Topping: " + topping + "</p>"
         var quantityAlert = "<p>Quantity: " + quantity + "</p>"
         var notesAlert = "<p>Notes: " + notesText + "</p>"
@@ -43,28 +47,37 @@ submitHandler = function (e) {
             + "\nTopping: " + topping
             + "\nNotes: " + notesText);
 
-        $("body").append(thankyou, toppingsAlert, quantityAlert, notesAlert);
+        // append the new message 
+        $("body").append(message, toppingsAlert, quantityAlert, notesAlert);
     }
+    // prevent original page from loading
     e.preventDefault();
 }
 
-// handler for each month
+/* Handler to help with the tasks needed to extract information for months*/
 monthHandler = function () {
+    // extract the current month text in dropdown
     var monthText = $(this).text();
     $("#monthText").html(monthText);
 
+    // send the document through post by the conditions of the function with the order array parameter
     $.post('\orders', function(orderArray) {
+        // hide the current orders
         $("#cherryOrders").hide();
         $("#chocolateOrders").hide();
         $("#orderList2").hide();
+
+        // determine the new orders
         var newCherryOrders = "<li>" + orderArray.data[0].cher.quantity + " " + orderArray.data[0].cher.topping + "</li>";
         var newChocolateOrders = "<li>" + orderArray.data[1].choc.quantity + " " + orderArray.data[1].choc.topping + "</li>";
         var newPlainOrders = "<li>" + orderArray.data[2].pla.quantity + " " + orderArray.data[2].pla.topping + "</li>";
+
+        // append the new orders to the page
         $("#orderList").append(newCherryOrders, newChocolateOrders, newPlainOrders);
     });
 }
 
-// read in and handle the events from the main document
+/* Read in and handle the events from the main document */
 $(document).ready(function () {
     // call submit on the form
     $("form").on("submit", submitHandler);
