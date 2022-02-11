a1d3a069c3c05ec0bfc8509838830057cf18f9f5
BH <74618590+harringt23@users.noreply.github.com>
2022-02-10 23:30:14 -0800

    ruh roh raggy

--- hw4/routes/neworder.js
+++ hw4/routes/neworder.js
@@ -19,7 +19,8 @@ var dbms = require('./dbms.js');
 // initialize function to insert orders
 function insert_order(orderid, month, day, quantity, topping, notes, callback) {
     dbms.dbquery("INSERT INTO ORDERS VALUES (" + orderid + ", \"" + month + "\", " + day + ", " + quantity + ", \"" + topping + "\", \"" + notes + "\");",
-        function (error, results) {
+        // print the information to the console for testing 
+        function(error, results) {
             if (error == false) {
                 console.log("[neworder.js] <insert_order> |  " + results);
             }
@@ -30,47 +31,24 @@ function insert_order(orderid, month, day, quantity, topping, notes, callback) {
 }
 
 // have the router use this to call next function
-router.use(function (req, res, next) {
+router.use(function(req, res, next) {
     console.log("[orders.js] <router.use>    |  Redirecting router... ");
     next();
 });
 
-// update the post function 
-// router.post('/', function(req, res) {
-//     // create a new ID
-//     var id = Math.floor(Math.random() * Math.floor(100));
-
-//     // initialize date as January, 25 
-//     var month = 'Jan';
-//     var day = 25;
-
-//     // extract the necessary information
-//     var quantity = req.body.quantity;
-//     var topping = req.body.topping;
-//     var notes = req.body.notes;
-
-//     // ensure the notes have a value
-//     if(notes == null) {
-//          notes = " ";
-//     }
-
-//     // insert the order values
-//     dbms.dbquery("INSERT INTO ORDERS VALUES ('" + id + "','" + month + "','" + day + "','" + quantity + "','" + topping + "','" + notes + "');", callBack);
-
-//     // error handling
-//     function callBack(error, result) {
-//         return;
-//     }
-//  });
-
-
-router.post('/', function (req, res, next) {
+// post the information 
+router.post('/', function(req, res, next) {
+    // get the date value 
     console.log("[neworder.js] <router.post> |  Retrieving request detail...");
     const orderid = (new Date().valueOf()) % 2147483646;
+
+    // get the month value 
     console.log("[neworder.js] <router.post> |  ...orderid created...");
     const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
     const month = months[Math.floor(Math.random() * 12)];
     console.log("[neworder.js] <router.post> |  ...month retrieved...");
+
+    // get the day value using spicy math
     var day_set;
     if (month == "SEP" || month == "APR" || month == "JUN" || month == "NOV") {
         day_set = Math.floor(Math.random() * 29) + 1;
@@ -79,13 +57,23 @@ router.post('/', function (req, res, next) {
     } else {
         day_set = Math.floor(Math.random() * 30) + 1;
     }
+
+    // set the day 
     const day = day_set;
     console.log("[neworder.js] <router.post> |  ...day retrieved...");
+
+    // set the quantity 
     const quantity = req.query.order.quantity;
     console.log("[neworder.js] <router.post> |  ...quantity retrieved...");
+
+    // set the topping
     const topping = ("" + req.query.order.topping).toUpperCase();
     console.log("[neworder.js] <router.post> |  ...topping retrieved...");
+
+    // set the notes
     const notes = req.query.order.notes;
+
+    // TESTING - print values to the console 
     console.log("[neworder.js] <router.post> |  ...notes retrieved...");
     console.log("[neworder.js] <router.post> |  All elements properly retrieved");
     console.log("[neworder.js] <router.post> |  Formatting an INSERT operation for:");
@@ -96,31 +84,31 @@ router.post('/', function (req, res, next) {
     console.log("                            .      " + topping + "\n                            .      ");
     console.log("                            .      " + notes + "\n                            .      ");
     console.log("[neworder.js] <router.post> |  Sending INSERT request...");
-    insert_order(orderid, month, day, quantity, topping, notes, function (error) {
+    insert_order(orderid, month, day, quantity, topping, notes, function(error) {
         res.send(error);
     });
-    // Enter callback hell
-    // get_num_orders("CHERRY", month, function(total_cherry) {
-    //     console.log("[neworder.js] <router.post> |  Returned number for cherry: " +total_cherry);
-    //     get_num_orders("PLAIN", month, function(total_plain) {
-    //         console.log("[neworder.js] <router.post> |  Returned number for plain: " +total_plain);
-    //         get_num_orders("CHOCOLATE", month, function(total_chocolate) {
-    //             console.log("[neworder.js] <router.post> |  Returned number for chocolate: " +total_chocolate);
-    //             var response = {
-    //                 "error":null,
-    //                 "data":[
-    //                         {"topping":"cherry", "quantity":total_cherry},
-    //                         {"topping":"plain", "quantity":total_plain},
-    //                         {"topping":"chocolate", "quantity":total_chocolate}
-    //                     ]
-    //             };
-    //             console.log("[neworder.js] <router.post> |  Client response prepared:");
-    //             console.log("                            .      " + JSON.stringify(response));
-    //             res.json(response);
-    //             console.log("[neworder.js] <router.post> |  JSON Response has been sent!");
-    //         });
-    //     });
-    // });
+    // testing of the absolute callback hell
+    get_num_orders("CHERRY", month, function(total_cherry) {
+        console.log("[neworder.js] <router.post> |  Returned number for cherry: " + total_cherry);
+        get_num_orders("PLAIN", month, function(total_plain) {
+            console.log("[neworder.js] <router.post> |  Returned number for plain: " + total_plain);
+            get_num_orders("CHOCOLATE", month, function(total_chocolate) {
+                console.log("[neworder.js] <router.post> |  Returned number for chocolate: " + total_chocolate);
+                var response = {
+                    "error": null,
+                    "data": [
+                        { "topping": "cherry", "quantity": total_cherry },
+                        { "topping": "plain", "quantity": total_plain },
+                        { "topping": "chocolate", "quantity": total_chocolate }
+                    ]
+                };
+                console.log("[neworder.js] <router.post> |  Client response prepared:");
+                console.log("                            .      " + JSON.stringify(response));
+                res.json(response);
+                console.log("[neworder.js] <router.post> |  JSON Response has been sent!");
+            });
+        });
+    });
 });
 
 module.exports = router;
--- hw4/routes/orders.js
+++ hw4/routes/orders.js
@@ -19,10 +19,10 @@ var dbms = require('./dbms.js');
 
 // function to get the number of orders 
 function get_num_orders(type, month, callback) {
-    dbms.dbquery("SELECT QUANTITY FROM ORDERS WHERE MONTH='"+ month +"' AND TOPPING='"+ type +"';",
-        function (error, results) {
+    dbms.dbquery("SELECT QUANTITY FROM ORDERS WHERE MONTH='" + month + "' AND TOPPING='" + type + "';",
+        function(error, results) {
             var total = 0;
-            if(error != false) {
+            if (error != false) {
                 total = "---";
                 // testing 
                 console.log("[orders.js] <dbms.dbquery>  |  Error! " + error);
@@ -31,7 +31,7 @@ function get_num_orders(type, month, callback) {
                     total = total + row.QUANTITY;
                 }
             }
-            console.log("[orders.js] <dbms.dbquery>  |  Result for '"+type+"' query: " + total);
+            console.log("[orders.js] <dbms.dbquery>  |  Result for '" + type + "' query: " + total);
             callback(total);
         }
     );
@@ -44,24 +44,25 @@ router.use(function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
+    // testing 
     console.log("[orders.js] <router.post>   |  Retrieving request detail")
     const month = req.query.month;
     console.log("                            .      " + month + "\n                            .      ");
     console.log("[orders.js] <router.post>   |  Querying database for quantities for month of '" + month + "'...");
-    // Enter callback hell
+    // enter the callback hell
     get_num_orders("CHERRY", month, function(total_cherry) {
-        console.log("[orders.js] <router.post>   |  Returned number for cherry: " +total_cherry);
+        console.log("[orders.js] <router.post>   |  Returned number for cherry: " + total_cherry);
         get_num_orders("PLAIN", month, function(total_plain) {
-            console.log("[orders.js] <router.post>   |  Returned number for plain: " +total_plain);
+            console.log("[orders.js] <router.post>   |  Returned number for plain: " + total_plain);
             get_num_orders("CHOCOLATE", month, function(total_chocolate) {
-                console.log("[orders.js] <router.post>   |  Returned number for chocolate: " +total_chocolate);
+                console.log("[orders.js] <router.post>   |  Returned number for chocolate: " + total_chocolate);
                 var response = {
-                    "error":null,
-                    "data":[
-                            {"topping":"cherry", "quantity":total_cherry},
-                            {"topping":"plain", "quantity":total_plain},
-                            {"topping":"chocolate", "quantity":total_chocolate}
-                        ]
+                    "error": null,
+                    "data": [
+                        { "topping": "cherry", "quantity": total_cherry },
+                        { "topping": "plain", "quantity": total_plain },
+                        { "topping": "chocolate", "quantity": total_chocolate }
+                    ]
                 };
                 console.log("[orders.js] <router.post>   |  Client response prepared:");
                 console.log("                            .      " + JSON.stringify(response));
