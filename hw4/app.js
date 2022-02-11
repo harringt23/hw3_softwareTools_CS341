b18a48d3a80689fea8960f617fdbc14976437675
BH <74618590+harringt23@users.noreply.github.com>
2022-02-10 22:51:54 -0800

    someone help

--- hw4/app.js
+++ hw4/app.js
@@ -14,8 +14,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+// initialize new routers
 var ordersRouter = require('./routes/orders');
-var process_ordersRouter = require('./routes/neworder');
+var neworderRouter = require('./routes/neworder');
 
 var app = express();
 
@@ -31,23 +33,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// require the app to use the new order routers
 app.use('/orders', ordersRouter);
-app.use('/process_orders', process_ordersRouter);
+app.use('/neworder', neworderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
